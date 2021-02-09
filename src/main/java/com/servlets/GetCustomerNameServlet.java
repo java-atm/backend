ec0bb8bc63dd29a9e9bbdcd4787d365d898feac6
Eduard Matveev <edo.matveev1996@mail.ru>
2021-02-09 23:58:09 +0400

    Example

--- src/main/java/com/servlets/GetCustomerNameServlet.java
+++ src/main/java/com/servlets/GetCustomerNameServlet.java
@@ -2,14 +2,14 @@ package com.servlets;
 
 
 import com.database_client.DatabaseClient;
+import com.utils.exceptions.ConnectionFailedException;
+import com.utils.exceptions.CustomerNotFoundException;
 import com.utils.exceptions.InvalidParameterException;
 import com.utils.exceptions.JSONParsingFailedException;
+import com.utils.readers.RequestReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
-import com.utils.readers.RequestReader;
-import com.utils.exceptions.ConnectionFailedException;
-import com.utils.exceptions.CustomerNotFoundException;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,54 +29,65 @@ public class GetCustomerNameServlet extends HttpServlet {
         JSONObject resultJson = new JSONObject();
         try (PrintWriter pr = response.getWriter()) {
             LOGGER.info("Parsing JSON");
-            JSONObject jsonObject;
-            try {
-                jsonObject = RequestReader.getJSONData(request);
-            } catch (JSONParsingFailedException e) {
-                response.setStatus(400);
-                resultJson.put("error", "Invalid JSON content");
-                pr.write(resultJson.toString());
-                pr.flush();
-                return;
-            }
-            LOGGER.info("JSON parsed, getting params: {}", jsonObject.toString());
-            try {
-                String atm_id = RequestReader.getATM_ID(jsonObject, "atm_id");
-                Long accountNumber = RequestReader.getAccountNumber(jsonObject, "accountNumber");
-                LOGGER.info("accountNumber and atm_id received, processing");
-                if (! DatabaseClient.verifyATMID(atm_id)) {
-                    LOGGER.error("atm_id {} not validated", atm_id);
-                    response.setStatus(403);
-                    resultJson.put("error", "ATM ID not found");
-                    pr.write(resultJson.toString());
-                    pr.flush();
-                    return;
-                }
-                LOGGER.info("atm_id validated, processing request");
-
-                String customerName= DatabaseClient.getCustomerFullNameByAccountNumber(accountNumber);
-                LOGGER.info("Got name: {} for {}", customerName, accountNumber);
-                resultJson.put("result", customerName);
-                pr.write(resultJson.toString());
-                pr.flush();
-            } catch (ConnectionFailedException | CustomerNotFoundException e) {
-                LOGGER.error("Something went wrong during processing: {}", e.getMessage(), e);
-                response.setStatus(202);
-                resultJson.put("error", e.getMessage());
-                pr.write(resultJson.toString());
-                pr.flush();
-            } catch (InvalidParameterException e) {
-                LOGGER.error("Invalid parameters: {}", e.getMessage(), e);
-                response.setStatus(400);
-                resultJson.put("error", e.getMessage());
-                pr.write(resultJson.toString());
-                pr.flush();
-            }
+            someFunction(request, response, pr);
         } catch (IOException e) {
             LOGGER.error("Something went wrong: {}", e.getMessage(), e);
             response.setStatus(500);
         }
     }
+
+    private void someFunction(HttpServletRequest request, HttpServletResponse response, PrintWriter pr) {
+        JSONObject resultJson = new JSONObject();
+        JSONObject jsonObject;
+        try {
+            jsonObject = RequestReader.getJSONData(request);
+        } catch (JSONParsingFailedException e) {
+            response.setStatus(400);
+            resultJson.put("error", "Invalid JSON content");
+            pr.write(resultJson.toString());
+            pr.flush();
+            return;
+        }
+        LOGGER.info("JSON parsed, getting params: {}", jsonObject.toString());
+        someFunction2(request, response, pr, jsonObject);
+    }
+
+    private void someFunction2(HttpServletRequest request, HttpServletResponse response, PrintWriter pr, JSONObject jsonObject) {
+        JSONObject resultJson = new JSONObject();
+        try {
+            String atm_id = RequestReader.getATM_ID(jsonObject, "atm_id");
+            Long accountNumber = RequestReader.getAccountNumber(jsonObject, "accountNumber");
+            LOGGER.info("accountNumber and atm_id received, processing");
+            if (! DatabaseClient.verifyATMID(atm_id)) {
+                LOGGER.error("atm_id {} not validated", atm_id);
+                response.setStatus(403);
+                resultJson.put("error", "ATM ID not found");
+                pr.write(resultJson.toString());
+                pr.flush();
+                return;
+            }
+            LOGGER.info("atm_id validated, processing request");
+
+            String customerName= DatabaseClient.getCustomerFullNameByAccountNumber(accountNumber);
+            LOGGER.info("Got name: {} for {}", customerName, accountNumber);
+            resultJson.put("result", customerName);
+            pr.write(resultJson.toString());
+            pr.flush();
+        } catch (ConnectionFailedException | CustomerNotFoundException e) {
+            LOGGER.error("Something went wrong during processing: {}", e.getMessage(), e);
+            response.setStatus(202);
+            resultJson.put("error", e.getMessage());
+            pr.write(resultJson.toString());
+            pr.flush();
+        } catch (InvalidParameterException e) {
+            LOGGER.error("Invalid parameters: {}", e.getMessage(), e);
+            response.setStatus(400);
+            resultJson.put("error", e.getMessage());
+            pr.write(resultJson.toString());
+            pr.flush();
+        }
+    }
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         LOGGER.info("GET from ADDR: {}", request.getRemoteAddr());
